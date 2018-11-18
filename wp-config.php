ec2d8f7e37646e9c04c855ba4e041c6b7b14a6d6
Russell Strauss <russellstrauss@gmail.com>
2018-11-17 21:33:30 -0500

    Adding database for backup

--- wp-config.php
+++ wp-config.php
@@ -18,23 +18,13 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
 define('DB_NAME', 'hammerhe_wrdp1');
-
-/** MySQL database username */
 define('DB_USER', 'hammerhe_wrdp1');
-
-/** MySQL database password */
 define('DB_PASSWORD', 'EZsDNwLGpIPKi4E');
-
-/** MySQL hostname */
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
-
-/** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
 /**#@+
