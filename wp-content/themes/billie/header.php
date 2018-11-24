a564633d365e94416c3acd1f5ed70def2f61762b
Russell Strauss <russellstrauss@gmail.com>
2018-11-24 01:08:06 -0500

    Improving fonts and profile photos

--- wp-content/themes/billie/header.php
+++ wp-content/themes/billie/header.php
@@ -15,6 +15,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600" rel="stylesheet">
 </head>
 
 <body <?php body_class(); ?>>
